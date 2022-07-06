371ba70302759393da5128724ed5e1d17b84bb52
ifeanyilucky <wfguru2017@gmail.com>
2022-07-06 17:12:13 +0100

    telegram link update

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config.address = '26, Boulevard Royal. Luxembourg 2449';
 config.linkedIn = 'https://www.linkedin.com/company/lemox_co';
 config.medium = 'https://medium.com/@lemox';
 config.facebook = 'https://www.facebook.com/Lemox-101380271875067/';
-config.telegram = 'https://t.me/+2V-pZb0MjvkwZGZk';
+config.telegram = 'https://t.me/lemoxco';
 config.twitter = 'https://twitter.com/Lemox_co';
 
 config.email.support = 'support@lemox.co';
